4afc12dc004491b64d047eb3fa32619ca39954c1
rturceaninov <43860405+rturceaninov@users.noreply.github.com>
2018-11-15 16:51:23 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+var promptX = prompt('Введите значение Х');
+var promptN = prompt('Введите значение N');
+document.getElementById('promptX').innerText = promptX;
+document.getElementById('promptN').innerText = promptN;
